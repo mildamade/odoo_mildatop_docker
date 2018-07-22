91fe682104e8125188d90ca7eaf96932558775cc
mildamade <milda.dvorak@gmail.com>
2018-07-22 20:54:03 +0200

    inicial

---
+++ backup.sh
@@ -0,0 +1,5 @@
+export PGPASSWORD="odoo" 
+docker exec -i odoomykkroczdocker_odoo11_1 /bin/bash -c \
+"export PGPASSWORD="odoo" \
+&& /usr/bin/pg_dump --host=685300d5b1be --dbname=MYKKRO --username=odoo" \
+> ./mykkrocz_postgresql.dump
